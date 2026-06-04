d9158aa47bd4278b18ddf8f21458c72bfdd155b8
Aditya <adityaupadhyay0906@gmail.com>
2026-06-04 09:56:50 +0530

    Initial Spring Boot setup and first endpoint

---
+++ src/main/java/com/kritikasharma/interview_prep_platform/InterviewPrepPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kritikasharma.interview_prep_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrepPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kritikasharma/interview_prep_platform/InterviewPrepPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kritikasharma.interview_prep_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
